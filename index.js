555c2ae59c47df178a9a76864c5e703cf99137d7
naresh7km <naresh7km@gmail.com>
2024-09-11 10:18:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const baseDomains = [
   "yumikoresorts.life",
   "tatsumionsen.site",
   "javhdaffiliates.site",
-  "seishinyogajp.com",
+  "seishinyogajp.fit",
   "hisashieats.com",
   "yogalex.site",
   "massagesayami.com",
