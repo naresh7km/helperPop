8c2ecdcd20db9d0307947b45583ea145376c4b70
naresh7km <naresh7km@gmail.com>
2024-09-12 13:08:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const baseDomains = [
   "sakurasuhiandramenbar.com",
   "yogametic.fit",
   "komorebiyoga.online",
-  "spa-delightjp.online",
+  "spa-delightjp.com",
   "yumikoresorts.life",
   "tatsumionsen.site",
   "javhdaffiliates.site",
