f81978010360b32a3c0b7e490df9b80e1e1098fd
naresh7km <naresh7km@gmail.com>
2024-09-21 19:05:25 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 const firstList = ["yuuyuuyoga", "yumikoresorts", "sakurasuhiandramenbar", "hanasakuniwa"];
-const secondList = ["komorebiyoga", "hikokimagic", "shinkansen"];
+const secondList = ["hikokimagic", "shinkansen"];
 const thirdList = ["spa-delightjp", "seishinyogajp", "tatsumionsen", "yogametic", "foodhutjapan", "hotelroyaltonjp"];
 const fourthList = ["hdjavforyou", "javhdaffiliates"];
 const fifthList = ["sakuracoffee", "yumimasseuse", "fitzonefitness", "shoppiejapanclothes", "osakastreetbites"];
@@ -16,7 +16,6 @@ const baseDomains = [
   "foodhutjapan.com",
   "yogametic.fit",
   "hotelroyaltonjp.com",
-  "komorebiyoga.online",
   "spa-delightjp.com",
   "yumikoresorts.life",
   "tatsumionsen.site",
@@ -33,8 +32,8 @@ const baseDomains = [
   "hanasakuniwa.shop",
   "shoppiejapanclothes.online",
   "fitzonefitness.live",
-  "shinkansen.info",
-  "hikokimagic.shop",
+  "shinkanseninfo.online",
+  "hikokimagic.store",
 ];
 
 // Generate all combinations of allowed URLs
