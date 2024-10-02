9af7602c25564bb35f0e2e5ad7155a7bdf548e2f
naresh7km <naresh7km@gmail.com>
2024-10-02 10:49:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const baseDomains = [
   "sakurasuhiandramenbar.com",
   "foodhutjapan.com",
   "hotelroyaltonjp.com",
-  "spadelightjapan.com",
+  "spadelightjapan.fit",
   "javhdaffiliates.site",
   "seishinyogajp.fit",
   "massagesayami.com",
