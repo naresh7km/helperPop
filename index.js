2970325841a5a6ab0450bf867cc548024484888b
naresh7km <naresh7km@gmail.com>
2024-11-23 20:15:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const secondList = ["hdjavforyou", "javhdaffiliates", "spadelightjapan"];
 const thirdList = ["seishinyoga", "yuyado-onsen", "spadelightjapan", "foodhutjapan", "hotelroyaltonjp"];
 const fourthList = ["tanenespa"];
 const fifthList = ["ruijiacoaching", "calmyogaandmeditation"];
-const sixthList = ["nursecares", "omnifoodss", "expressmealdelivery", "yogalhealing", "gourmethubeats", "instanttnews"];
+const sixthList = ["nursecares", "omnifoodss", "expressmealdelivery", "yogalhealing", "gourmethubeats"];
 
 // Base domains for allowed origins and referrers
 const baseDomains = [
@@ -23,16 +23,12 @@ const baseDomains = [
   "seishinyoga.com",
   "hdjavforyou.online",
   "yumimasseuse.shop",
-  "hanasakuniwa.shop",
-  "nogamilearnings.shop",
-  "fitclubcommunity.shop",
   "expressmealdelivery.shop",
   "hanasakuniwa.shop",
   "omnifoodss.shop",
   "gourmethubeats.shop",
   "yogahealing.netlify.app",
   "nursecares.netlify.app",
-  "instanttnews.shop",
   "ruijiacoaching.us",
   "calmyogaandmeditation.online",
 ];
