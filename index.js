6b9cd5223b86937bdb339cecced9d36b5e2e28e7
naresh7km <naresh7km@gmail.com>
2025-05-28 13:05:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ const PORT = process.env.PORT || 3000;
 const officeList1 = ["hdjavforyou", "javhdaffiliates"];
 
 // For tuning OFF - dmclist1
-const dmcList1 = [];
+const dmcList1 = ["seishinyoga"];
 
 
 
 
 // For tuning ON - dmclist3
-const dmcList3 = ["seishinyoga"];
+const dmcList3 = [];
 
 
 
