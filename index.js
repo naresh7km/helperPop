1afeffee3eadd5d912b6e2804964434a93d99a52
naresh7km <naresh7km@gmail.com>
2025-06-03 05:51:42 +0900

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const wayneList2 = ["conversationseattle.shop"];
 
 // redundant lists for now - DON'T USE !!!
 const wayneList1 = [];
-const wayneList3 = ["newgenpoptesting.netlify.app", "127.0.0.1:5500"];
+const wayneList3 = ["127.0.0.1:5500", "kevinsfoodhut.store"];
 const wayneList4 = [];
 const wayneList5 = [];
 
@@ -55,8 +55,8 @@ const baseDomains = [
   "yogahealing.netlify.app",
   "gourmethub.netlify.app",
   "conversationseattle.shop",
-  "newgenpoptesting.netlify.app",
   "127.0.0.1:5500",
+ "kevinsfoodhut.store",
 ];
 
 // Generate all combinations of allowed URLs
