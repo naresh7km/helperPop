c17abf34ecf9d51e272344474a327c48e3fd771c
naresh7km <naresh7km@gmail.com>
2025-09-17 16:23:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 const officeList1 = ["hdjavforyou", "javhdaffiliates"];
 
 // For Turning OFF
-const dmcList1 = [];
+const dmcList1 = ["seishinyoga"];
 
 
 // For OFFICE
@@ -15,7 +15,7 @@ const dmcList3 = [];
 
 
 // For LUPIN 
-const dmcList2 = ["seishinyoga"];
+const dmcList2 = [];
 
 
 
